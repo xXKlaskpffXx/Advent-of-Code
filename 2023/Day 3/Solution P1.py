df4b04c11cba168d188c7c427049893144dc8d1b
xXKlaskpffXx <53981294+xXKlaskpffXx@users.noreply.github.com>
2023-12-03 16:33:40 +0100

    save

--- 2023/Day 3/Solution P1.py
+++ 2023/Day 3/Solution P1.py
@@ -1,5 +1,5 @@
 data = open('2023\Day 3\Input.in').read().split('\n')
-zeichen = [[],[]]
+characters = [[],[]]
 values = [[],[],[]]
 x = 0
 y = 0
@@ -14,6 +14,16 @@ def checkifvalid(valx, valy, zx, zy, val):
         if dy <= 1 and dy >= -1:
             return True
 
+def writevalue(list, x, y, val):
+    list[0].append(x-1)
+    list[1].append(y)
+    list[2].append(val)
+    return list, 0
+
+def writecharacter(list, x, y):
+    list[0].append(x)
+    list[1].append(y)
+    return list
 
 
 for i in data:
@@ -22,33 +32,20 @@ for i in data:
             if j.isnumeric():
                 value = int(str(value) + j)
             else:
-                zeichen[0].append(x)
-                zeichen[1].append(y)
-                if not value == 0: 
-                    values[0].append(x-1)
-                    values[1].append(y)
-                    values[2].append(value)
-                    value = 0
+                characters = writecharacter(characters, x, y)
+                if not value == 0: values, value = writevalue(values, x, y, value)
         else:
-            if not value == 0: 
-                values[0].append(x-1)
-                values[1].append(y)
-                values[2].append(value)
-                value = 0
+            if not value == 0: values, value = writevalue(values, x, y, value)
         x += 1
-    if not value == 0: 
-        values[0].append(x-1)
-        values[1].append(y)
-        values[2].append(value)
-        value = 0
+    if not value == 0: values, value = writevalue(values, x, y, value)
     x = 0
     y += 1
 y = 0
 
 
 for i in range(len(values[2])):
-    for j in range(len(zeichen[0])):
-        if checkifvalid(values[0][i], values[1][i], zeichen[0][j], zeichen[1][j], values[2][i]): 
+    for j in range(len(characters[0])):
+        if checkifvalid(values[0][i], values[1][i], characters[0][j], characters[1][j], values[2][i]): 
             sum += values[2][i]
             break
 
